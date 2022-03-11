159fcbd32ca51408b99b61b9da4f227f115bcde2
MarcoLorenzetti <marco.lorenzetti.1@studenti.unipd.it>
2022-03-11 18:34:49 +0100

    Marco's analysis - 4 different nonlinearfuntion

--- nonlinear_function.py
+++ nonlinear_function.py
@@ -3,11 +3,17 @@ import numpy as np
 # a nonlinear function of a 2d array x
 def f(x,c=1):
     r=0
-    if c==1:
+    if c==1: #TRIANGLE
         if x[0]>-20 and x[1]>-40 and x[0]+x[1] < 40:
             r=1
-    if c==2:
+    if c==2: #COMPLEX SYMMETRIC CIRCULAR
         if (np.sign(x.sum())*np.sign(x[0]))*np.cos(np.linalg.norm(x)/(2*np.pi))>0:
+            r=1
+    if c==3: #CIRCLE
+        if (x[0]**2 + x[1]**2)<400:
+            r=1
+    if c==4: #FLAG DESIGN
+        if 40*np.sin(x[0]) < x[1]:
             r=1
     return r
 
